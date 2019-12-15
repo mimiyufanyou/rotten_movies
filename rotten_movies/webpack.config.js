6f64cf78c372555174a95d5e1dc3118b1df4e512
Mimi <yufan.you@gmail.com>
2019-12-14 16:33:48 -0800

    test vs code

---
+++ rotten_movies/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: '/Users/mimiyufanyou/Documents/GitHub/rotten_movies/rotten_movies/src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
